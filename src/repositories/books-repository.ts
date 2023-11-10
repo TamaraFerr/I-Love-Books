c46f2575e6b8bbaa0ae08842dcc89c7031823852
TamaraFerr <tamarablacksmith@hotmail.com>
2023-11-10 15:34:14 -0300

    FEAT: triedto fix code

--- src/repositories/books-repository.ts
+++ src/repositories/books-repository.ts
@@ -23,8 +23,21 @@ export async function createBook(book: CreateBook) {
 }
 
 export async function reviewBook(bookReview: CreateReview) {
-  const result = await prisma.books.create({
-    data: bookReview
-  })
-  return result;
+  const { bookId, grade, review } = bookReview;
+  const query = `
+    UPDATE books 
+    SET
+      grade = $1,
+      review = $2,
+      read = true 
+    WHERE id = $3
+  `;
+
+  const result = await connection.query(query, [grade, review, bookId]);
+  return result.rowCount;
+
+  // const result = await prisma.books.create({
+  //   data: bookReview
+  // })
+  // return result;
 }
